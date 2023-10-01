f1c84772af85a6de71129a3ecdb01a94cab5f440
witchlad <dolearczyk95@gmail.com>
2023-10-01 09:32:15 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,26 @@
+//Navigation
 jQuery(document).ready(function ($) {
     $("#nav-burger").on("click", function () {
       $("#nav-menu").toggleClass("navigation__nav-show", 500);
     });
 });
 
+//Links
+jQuery(document).ready(function ($) {
+    $(document).on("click", 'a[href^="#"]', function (event) {
+        event.preventDefault();
+    
+        $("html, body").animate(
+          {
+            scrollTop: $($.attr(this, "href")).offset().top,
+          },
+          500,
+        );
+    });    
+});
+
+
+//Swiper
 var swiper = new Swiper(".mySwiper-projekty", {
   navigation: {
       nextEl: ".swiper-button-next",
